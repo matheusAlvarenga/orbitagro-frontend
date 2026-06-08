27cd8d9aabd416f166988ff674ce9b255dba08bb
Matheus Alvarenga <matheus.alvarenga0302@gmail.com>
2026-06-08 13:35:12 -0300

    chore: add EditorConfig and Biome configuration files, update package.json and pnpm-lock.yaml for Biome integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
 export const App = () => {
-  return (
-    <>
-      <h1>Orbitagro</h1>
-    </>
-  )
-}
+	return <h1>Orbitagro</h1>;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {App} from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// biome-ignore lint/style/noNonNullAssertion: the root element is guaranteed to exist
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
